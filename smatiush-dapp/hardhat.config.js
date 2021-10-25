cf1e6bea21232577b7642190b9403b130e602282
smatiush <smatiushtechnology@gmail.com>
2021-10-25 09:38:22 +0200

    Add files via upload

---
+++ smatiush-dapp/hardhat.config.js
@@ -0,0 +1,33 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.3",
+    paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/3d1d2343f9994001a3876a91ad7ef4cf",
+      accounts: ['377d28625eea5903c4f524efda9080861f3c751214d7e1df1ce74908657f4799']
+    }
+  }
+};
---
+++ smatiush-dapp/scripts/deploy.js
@@ -0,0 +1,29 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
+
+  const Greeter = await hre.ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, World!");
+
+  const Token = await hre.ethers.getContractFactory("SpoonToken");
+  const token = await Token.deploy("spoon token ERC20", "spn");
+
+  await greeter.deployed();
+  await token.deployed();
+
+  console.log("Greeter deployed to:", greeter.address);
+  console.log("Token deployed to:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
